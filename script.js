c279952575b1578cf7e8fcb76d108ba3c9e090fe
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-29 20:01:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ window.promises = [];
 // Do not change the code above this
 // add your promises to the array `promises`
 let result = document.getElementBYId("output")
-let p1 = new Promise(resolve() ,reject());
-let p2 = new Promise(resolve() ,reject());
-let p3 = new Promise(resolve() ,reject());
-let p4 = new Promise(resolve() ,reject());
-let p5 = new Promise(resolve() ,reject());
-result.innerHTML = Promise.any(p1, p2, p3, p4, p5)
+let prom1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve();
+    }, 3000)
+});
+let prom2 = new Promise((resolve,reject) => {
+	setTimeout(() =>{
+		resolve()
+	},1000)
+})
+let prom3 = new Promise((resolve , reject) () => {
+	setTimeout(() => {
+		resolue()
+	},5000)
+});
+let prom4 = new Promise((resolve , reject) () => {
+	setTimeout(() => {
+		resolue()
+	},5000)
+});
+let prom5 = new Promise((resolve , reject) () => {
+	setTimeout(() => {
+		resolue()
+	},5000)
+});
+result.innerHTML = Promise.any(prom1, prom2, prom3, prom4, prom5)
