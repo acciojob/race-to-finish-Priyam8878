72cdcfa210f1ddb991d4ab97e0de50e7ca6bec5d
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-29 21:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let result = document.getElementBYId("output")
+let result = document.getElementById("output")
+
 let prom1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("promise 1 resolve");
@@ -13,17 +14,17 @@ let prom2 = new Promise((resolve,reject) => {
 		resolve("promise 2 resolve")
 	},1000)
 })
-let prom3 = new Promise((resolve , reject) () => {
+let prom3 = new Promise((resolve , reject)  => {
 	setTimeout(() => {
 		resolve("promise 3 resolve")
 	},5000)
 });
-let prom4 = new Promise((resolve , reject) () => {
+let prom4 = new Promise((resolve , reject)  => {
 	setTimeout(() => {
 		resolve("promise 4 resolve")
 	},5000)
 });
-let prom5 = new Promise((resolve , reject) () => {
+let prom5 = new Promise((resolve , reject)  => {
 	setTimeout(() => {
 		resolve("promise 5 resolve")
 	},5000)
